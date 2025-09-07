37e28f8f4a04a3dd58b7c62cab392d89a4e64d17
Tarun Kumar Dewangan <raj.dewangan97@gmail.com>
2025-09-07 20:03:12 +0530

    every minute test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,13 +7,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    //->dailyAt('09:00')
     protected $commands = [
         Commands\SendExpiryNotifications::class,
     ];
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('notifications:send-expiries')
-            ->dailyAt('09:00')
+            ->everyMinute()
             ->timezone('Asia/Kolkata')
             ->appendOutputTo(storage_path('logs/scheduler.log'))
             ->emailOutputOnFailure('your-email@example.com'); // Optional
