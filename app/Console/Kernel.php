c3876cd192e086cf541cb258edbb9ebb2ea885f8
Tarun Kumar Dewangan <raj.dewangan97@gmail.com>
2025-09-27 16:14:04 +0530

    after fixing cron jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('notifications:send-expiries')
-            ->everyMinute() //->dailyAt('09:00')
+            ->dailyAt('09:00')    //->everyMinute()
             ->timezone('Asia/Kolkata')
             ->appendOutputTo(storage_path('logs/scheduler.log'))
             ->before(function () {
